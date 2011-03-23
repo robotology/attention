56f839994f18bf9726156b2d8ba6d917abb51d45
Shashank Pathak <shashank.pathak@iit.it>
2011-03-23 16:07:02 +0000

    adding source files

---
+++ src/mosaic/src/mosaicModule.cpp
@@ -0,0 +1,144 @@
+/* 
+ * Copyright (C) 2011 RobotCub Consortium, European Commission FP6 Project IST-004370
+ * Authors: Shashank Pathak
+ * email:   shashank.pathak@iit.it
+ * website: www.robotcub.org 
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+ */
+
+/**
+ * @file mosaicModule.cpp
+ * @brief Implementation of the mosaicModule (see header file).
+ */
+
+#include "iCub/mosaicModule.h"
+
+using namespace yarp::os;
+using namespace yarp::sig;
+using namespace std;
+
+/* 
+ * Configure method. Receive a previously initialized
+ * resource finder object. Use it to configure your module.
+ * If you are migrating from the old Module, this is the 
+ *  equivalent of the "open" method.
+ */
+
+bool mosaicModule::configure(yarp::os::ResourceFinder &rf) {
+    /* Process all parameters from both command-line and .ini file */
+
+    /* get the module name which will form the stem of all module port names */
+    moduleName            = rf.check("name", 
+                           Value("/mosaic"), 
+                           "module name (string)").asString();
+    /*
+    * before continuing, set the module name before getting any other parameters, 
+    * specifically the port names which are dependent on the module name
+    */
+    setName(moduleName.c_str());
+
+    /*
+    * get the robot name which will form the stem of the robot ports names
+    * and append the specific part and device required
+    */
+    robotName             = rf.check("robot", 
+                           Value("icub"), 
+                           "Robot name (string)").asString();
+    robotPortName         = "/" + robotName + "/head";
+
+    /*
+    * attach a port of the same name as the module (prefixed with a /) to the module
+    * so that messages received from the port are redirected to the respond method
+    */
+    handlerPortName =  "";
+    handlerPortName += getName();         // use getName() rather than a literal 
+
+    if (!handlerPort.open(handlerPortName.c_str())) {           
+        cout << getName() << ": Unable to open port " << handlerPortName << endl;  
+        return false;
+    }
+
+    attach(handlerPort);                  // attach to port
+
+    /* create the thread and pass pointers to the module parameters */
+    mThread = new mosaicThread();
+    mThread->setName(getName().c_str());
+    
+    /* now start the thread to do the work */
+    mThread->start(); // this calls threadInit() and it if returns true, it then calls run()
+
+    return true ;       // let the RFModule know everything went well
+                        // so that it will then run the module
+}
+
+bool mosaicModule::interruptModule()
+{
+    handlerPort.interrupt();
+    return true;
+}
+
+bool mosaicModule::close()
+{
+    handlerPort.close();
+    /* stop the thread */
+    printf("stopping the thread \n");
+    mThread->stop();
+    return true;
+}
+
+bool mosaicModule::respond(const Bottle& command, Bottle& reply) 
+{
+	string helpMessage =  string(getName().c_str()) + 
+		        " commands are: \n" +  
+		        "help \n" +
+			"size <int> <int> -to change, if allowed, size (width, height) of mosaic to <int> <int> \n" + 
+			"place <int> -to place, if allowed, input image's center (horz, vert) in mosaic refernce frame \n" +
+		        "quit \n";
+
+	reply.clear(); 
+
+
+	if (command.get(0).asString()=="quit") {
+		reply.addString("quitting");
+		return false;     
+	}
+	else if (command.get(0).asString()=="help") {
+		cout << helpMessage;
+		reply.addString("ok");
+	}
+	else if (command.get(0).asString() == "size") {
+		bool set = mThread->setMosaicSize(command.get(1).asInt(), command.get(2).asInt());
+		if (set) reply.addString("Changed size of the mosaic.");
+		else reply.addString("Could NOT change size of the mosaic.");
+	}
+	else if (command.get(0).asString() == "place") {
+		bool set = mThread->placeInpImage(command.get(1).asInt(), command.get(2).asInt());
+		if(set) reply.addString("Input image placed successfully");
+		else reply.addString("Input image can NOT be placed there!");
+	}
+
+    return true;
+}
+
+/* Called periodically every getPeriod() seconds */
+bool mosaicModule::updateModule()
+{
+    return true;
+}
+
+double mosaicModule::getPeriod()
+{
+    /* module periodicity (seconds), called implicitly by myModule */
+    return 0.1;
+}
+
---
+++ src/mosaic/src/mosaicThread.cpp
@@ -0,0 +1,177 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/* 
+ * Copyright (C) 2011 RobotCub Consortium, European Commission FP6 Project IST-004370
+ * Authors: Shashank Pathak
+ * email:   shashank.pathak@iit.it
+ * website: www.robotcub.org 
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+ */
+
+/**
+ * @file mosaicThread.cpp
+ * @brief Implementation of the mosaic thread (see mosaicThread.h).
+ */
+
+#include <iCub/mosaicThread.h>
+#include <cstring>
+
+
+using namespace yarp::os;
+using namespace yarp::sig;
+using namespace std;
+
+
+
+mosaicThread::mosaicThread() {
+
+    inputImage = new ImageOf<PixelRgb>;
+    outputImageMosaic = new ImageOf<PixelRgb>;
+
+    //input image default size
+    width_orig = 320;
+    height_orig = 240;
+    
+    //mosaic image default size
+    width = 640;
+    height = 480;
+
+    //default position of input image's center
+    xcoord = width/2;
+    ycoord = height/2;
+    
+    resized = false;
+
+}
+
+mosaicThread::~mosaicThread() {
+    // do nothing   
+}
+
+bool mosaicThread::threadInit() {
+    /* open ports */ 
+    if (!imagePortIn.open(getName("/image:i").c_str())) {
+        cout <<": unable to open port for camera  "  << endl;
+        return false;  // unable to open; let RFModule know so that it won't run
+    }
+
+    
+    if (!imagePortOut.open(getName("/image:o").c_str())) {
+        cout << ": unable to open port "  << endl;
+        return false;  // unable to open; let RFModule know so that it won't run
+    }
+    
+    return true;
+}
+
+
+void mosaicThread::setName(string str) {
+    this->name=str;
+    printf("name: %s", name.c_str());
+}
+
+
+
+std::string mosaicThread::getName(const char* p) {
+    string str(name);
+    str.append(p);
+    return str;
+}
+
+void mosaicThread::run() {
+    
+    outputImageMosaic->resize(this->width,this->height);
+    outputImageMosaic->zero();
+    while (isStopping() != true)  {                
+        
+        inputImage = imagePortIn.read(false); // do not wait                           
+
+        if (inputImage != NULL ) {            
+            if(!resized) {
+                resize(inputImage->width(),inputImage->height());
+                resized = true;
+            }
+            makeMosaic(inputImage); 
+            imagePortOut.prepare() = *outputImageMosaic;                   
+            imagePortOut.write();
+        }                      
+    }
+}
+
+void mosaicThread::resize(int width_orig,int height_orig) {        
+    inputImage->resize(width_orig,height_orig);
+    this->width_orig = width_orig;
+    this->height_orig = height_orig;
+    
+}
+
+bool mosaicThread::placeInpImage(int X, int Y) {
+    if(X > this->width || X < 0 || Y > this->height || Y < 0) return false;
+    this->xcoord = X;
+    this->ycoord = Y;
+    return true;
+}
+
+bool mosaicThread::setMosaicSize(int width=DEFAULT_WIDTH, int height=DEFAULT_HEIGHT) {
+    if(width > MAX_WIDTH || width < width_orig || width <= 0 
+       || height > MAX_HEIGHT || height < height_orig || height <= 0 ) return false;
+    this->width = width;
+    this->height = height;
+    outputImageMosaic->resize(this->width,this->height);
+    outputImageMosaic->zero();
+    return true;    
+}
+
+void mosaicThread::makeMosaic(ImageOf<PixelRgb>* inputImage) {
+    int i,j;
+    unsigned char* inpTemp = inputImage->getRawImage();
+    unsigned char* outTemp = outputImageMosaic->getRawImage();
+    int iW = inputImage->width();
+    int iH = inputImage->height();
+    int mPad = outputImageMosaic->getPadding();
+    float lambda = .8;
+    
+    for(i = 0 ; i < iH ; ++i) {
+        for(j = 0 ; j < iW ; ++j) {
+            int mosaicX = i; mosaicX -= iH / 2 ; mosaicX += ycoord; 
+            int mosaicY = j; mosaicY -= iW / 2 ; mosaicY += xcoord;
+            if(mosaicX < height && mosaicY < width) {
+                int index = mosaicX; index *= width * 3 + mPad; index += 3 * mosaicY;
+                *(outTemp + index) *= lambda; 
+                *(outTemp + index) += *inpTemp;
+                inpTemp++;
+                *(outTemp + index + 1) *= lambda;
+                *(outTemp + index + 1) += *inpTemp;
+                inpTemp++;
+                *(outTemp + index + 2) *= lambda;
+                *(outTemp + index + 2) += *inpTemp;
+                inpTemp++;
+            }
+            else inpTemp +=3;
+        }
+    }
+}
+
+
+void mosaicThread::threadRelease() {
+    resized = false;     
+}
+
+void mosaicThread::onStop() {
+    imagePortIn.interrupt();
+    imagePortOut.interrupt();
+        
+    imagePortOut.close();
+    imagePortIn.close();
+}
+
