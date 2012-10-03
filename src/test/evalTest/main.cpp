2200909cf103bf84eff7280d59b20c2bc0da82ca
Francesco Rea <francesco.rea@iit.it>
2012-10-03 15:55:39 +0000

    corrected version without double wait for the data

--- src/test/evalTest/main.cpp
+++ src/test/evalTest/main.cpp
@@ -29,6 +29,7 @@
 #include <yarp/math/Math.h>
 #include <yarp/math/Rand.h>
 #include <iCub/ctrl/kalman.h>
+#include <iCub/attention/predModels.h>
 #include <iCub/attention/evalThread.h>
 // #include <yarp/os/Port.h>
 
@@ -45,6 +46,42 @@ using namespace attention::evaluator;
      Network yarp;
      Time::turboBoost();
 
+     Semaphore s;
+     evalThread* eval;
+     /*
+     linAccModel* modelB = new linAccModel();
+     modelB->init(1.0);
+     int rowA = modelB->getA().rows();
+     int colA = modelB->getA().cols();
+     Vector z0(rowA);
+     Vector x0(rowA);
+     x0.zero();z0.zero();
+     x0(0) = 1.0; 
+     Matrix P0(rowA,colA);
+     printf("initialisation of P0 %d %d \n", rowA, colA);
+     for (int i = 0; i < rowA; i++) {
+         for (int j = 0; j < colA; j++) { 
+             P0(i,j) += 0.01;
+         }      
+     }
+     printf("modelB\n %s \n %s \n", modelB->getA().toString().c_str(),modelB->getB().toString().c_str());    
+     printf("P0\n %s \n", P0.toString().c_str());    
+     genPredModel* mB = dynamic_cast<genPredModel*>(modelB);
+     */
+     
+     
+     printf(" creating eval thread \n");
+     eval = new evalThread();
+     //eval->init(z0,x0,P0);
+     //printf("genPred model A \n %s \n",mB    ->getA().toString().c_str());
+     //printf("lin acc model A \n %s \n",modelB->getA().toString().c_str());
+     //printf("just initialised genPredModel %08X \n",&eval);
+     s.wait();
+     eval->start();
+     s.post();
+     
+     printf("Press CTRL-C to stop the module \n");
+     while(true) {}
      
      Network::fini();
     
