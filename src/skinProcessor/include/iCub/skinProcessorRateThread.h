8b15e57f13fcfc6470aaaba497e2ff62eb637104
fvannu <sl_genoa-fv@hotmail.it>
2017-01-23 11:41:16 +0100

    skin processor module with body part choice

---
+++ src/skinProcessor/include/iCub/skinProcessorRateThread.h
@@ -0,0 +1,147 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+  * Copyright (C)2016  Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+  * Author:Fabio Vannucci
+  * email: fabio.vannucci@iit.it
+  * Permission is granted to copy, distribute, and/or modify this program
+  * under the terms of the GNU General Public License, version 2 or any
+  * later version published by the Free Software Foundation.
+  *
+  * A copy of the license can be found at
+  * http://www.robotcub.org/icub/license/gpl.txt
+  *
+  * This program is distributed in the hope that it will be useful, but
+  * WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+  * Public License for more details
+*/
+
+
+/**
+ * @file skinProcessorThread.h
+ * @brief Definition of a thread that receives input from skin and outputs message strings on a port
+ */
+
+
+#ifndef _SKINPROCESSOR_RATETHREAD_H_
+#define _SKINPROCESSOR_RATETHREAD_H_
+
+#include <yarp/sig/all.h>
+#include <yarp/os/all.h>
+#include <yarp/dev/all.h>
+#include <yarp/os/RateThread.h>
+#include <iCub/skinDynLib/skinContactList.h>
+#include <iCub/skinDynLib/skinContact.h>
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include <stdio.h>
+#include <cv.h>
+#include <cxcore.h>
+#include <highgui.h>
+
+
+
+class skinProcessorRateThread : public yarp::os::RateThread {
+private:
+    bool idle;                      // flag that interrupts the processing
+    int c;                          // character read from keyboard
+    int count[20];
+
+    std::string robot;              // name of the robot
+    std::string configFile;         // name of the configFile where the parameter of the camera are set
+    int useLeftArm;
+    int useRightArm;
+    int useTorso;
+
+    yarp::os::Bottle* inputValue;
+    yarp::os::Bottle* contactList;
+    int bodyPart;
+    int touchedPart;
+    std::vector<int> taxellList;
+    std::vector<int> touchInfo;
+    std::vector<double> geometricCenter;
+    double avgPressure;
+
+    cv::Mat image;
+    yarp::sig::ImageOf<yarp::sig::PixelRgb>* outputImage;                    
+
+    yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelRgb> > outputImagePort;     // output port , result of the processing
+    yarp::os::BufferedPort<yarp::os::Bottle> outputDataPort;
+    yarp::os::BufferedPort<yarp::os::Bottle> inputPort;
+    std::string name;                                        // rootname of all the ports opened by this thread
+
+    
+public:
+    /**
+    * constructor default
+    */
+    skinProcessorRateThread();
+
+    /**
+    * constructor 
+    * @param robotname name of the robot
+    */
+    skinProcessorRateThread(std::string robotname, std::string configFile);
+
+    /**
+     * destructor
+     */
+    ~skinProcessorRateThread();
+
+    /**
+    *  initialises the thread
+    */
+    bool threadInit();
+
+    /**
+    *  correctly releases the thread
+    */
+    void threadRelease();
+
+    /**
+    *  active part of the thread
+    */
+    void run(); 
+
+    /**
+    *  on stopping of the thread
+    */
+    void onStop();
+
+    /*
+    * function that sets the rootname of all the ports that are going to be created by the thread
+    * @param str rootnma
+    */
+    void setName(std::string str);
+
+    /**
+     * @brief function to set the body parts to consider in the thread
+     */
+    void setBodyParts(int left, int right, int torso);
+    
+    /**
+    * function that returns the original root name and appends another string iff passed as parameter
+    * @param p pointer to the string that has to be added
+    * @return rootname 
+    */
+    std::string getName(const char* p);
+
+
+    /**
+     * @brief function of main processing in the thread
+     */
+    int processing();
+
+
+     /**
+    *  draw the rectangles for contact
+    */
+    void drawContact(); 
+};
+
+#endif  //_SKINPROCESSOR_RATETHREAD_H_
+
+//----- end-of-file --- ( next line intentionally left blank ) ------------------
+
---
+++ src/skinProcessor/src/skinProcessorRateThread.cpp
@@ -0,0 +1,326 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+  * Copyright (C)2016  Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+  * Author:Fabio Vannucci
+  * email: fabio.vannucci@iit.it
+  * Permission is granted to copy, distribute, and/or modify this program
+  * under the terms of the GNU General Public License, version 2 or any
+  * later version published by the Free Software Foundation.
+  *
+  * A copy of the license can be found at
+  * http://www.robotcub.org/icub/license/gpl.txt
+  *
+  * This program is distributed in the hope that it will be useful, but
+  * WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+  * Public License for more details
+*/
+
+/**
+ * @file skinProcessorThread.cpp
+ * @brief Implementation of the skin thread (see skinProcessorThread.h).
+ */
+
+#include <iCub/skinProcessorRateThread.h>
+#include <cstring>
+
+using namespace yarp::dev;
+using namespace yarp::os;
+using namespace yarp::sig;
+using namespace iCub::skinDynLib;
+using namespace std;
+using namespace cv;
+
+///// dictionary
+
+#define VOCAB_TORSO_UPRIGHT              VOCAB3('t','u','r')
+#define VOCAB_TORSO_UPLEFT               VOCAB3('t','u','l')
+#define VOCAB_TORSO_DOWNRIGHT            VOCAB3('t','d','r')
+#define VOCAB_TORSO_DOWNLEFT             VOCAB3('t','d','l')
+#define VOCAB_RIGHTARM_UPRIGHT           VOCAB3('r','u','r')
+#define VOCAB_RIGHTARM_UPLEFT            VOCAB3('r','u','l')
+#define VOCAB_RIGHTARM_FORERIGHT         VOCAB3('r','f','r')
+#define VOCAB_RIGHTARM_FORELEFT          VOCAB3('r','f','l')
+#define VOCAB_RIGHTARM_HAND              VOCAB2('r','h')
+#define VOCAB_LEFTARM_UPRIGHT            VOCAB3('l','u','r')
+#define VOCAB_LEFTARM_UPLEFT             VOCAB3('l','u','l')
+#define VOCAB_LEFTARM_FORERIGHT          VOCAB3('l','f','r')
+#define VOCAB_LEFTARM_FORELEFT           VOCAB3('l','f','l')
+#define VOCAB_LEFTARM_HAND              VOCAB2('l','h')
+
+//////
+
+#define THRATE 33 //ms
+
+skinProcessorRateThread::skinProcessorRateThread():RateThread(THRATE) {
+    robot = "icub";
+}
+
+skinProcessorRateThread::skinProcessorRateThread(string _robot , string _configFile):RateThread(THRATE){
+    robot = _robot;
+    configFile = _configFile;
+}
+
+skinProcessorRateThread::~skinProcessorRateThread() {
+    // do nothing
+}
+
+bool skinProcessorRateThread::threadInit() {
+    //init variables
+    bodyPart = 0;
+    touchedPart = 0;
+    avgPressure = 0.0;
+    taxellList.resize(0);
+    geometricCenter.resize(0);
+    touchInfo.resize(14);
+    for(int i=0; i<14;i++){
+        touchInfo[i] = 0;
+    }
+    
+    image = Mat::zeros( 600, 600, CV_8UC3 );
+    outputImage      = new ImageOf<PixelRgb>;
+    outputImage->resize(600,600);
+
+    // opening the port for direct input
+    if (!inputPort.open(getName("/skinTouch:i").c_str())) {                                 
+        yError("unable to open port to receive input");
+        return false;  // unable to open; let RFModule know so that it won't run
+    }
+
+    // opening the port for direct output
+    if (!outputImagePort.open(getName("/skinTouch/image:o").c_str())) {
+        yError("unable to open port to send unmasked events ");
+        return false;  // unable to open; let RFModule know so that it won't run
+    }   
+    
+    if (!outputDataPort.open(getName("/skinTouch/data:o").c_str())) {
+        yError("unable to open port to send unmasked events ");
+        return false;  // unable to open; let RFModule know so that it won't run
+    }     
+
+    yInfo("Initialization of the processing thread correctly ended");
+
+    return true;
+}
+
+void skinProcessorRateThread::setName(string str) {
+    this->name=str;
+}
+
+void skinProcessorRateThread::setBodyParts(int _left, int _right, int _torso) {
+    useLeftArm = _left;
+    useRightArm = _right;
+    useTorso = _torso;
+}
+
+
+std::string skinProcessorRateThread::getName(const char* p) {
+    string str(name);
+    str.append(p);
+    return str;
+}
+
+void skinProcessorRateThread::run() {
+        if (inputPort.getInputCount()) {
+            inputValue = inputPort.read(false);                                                     //non blocking read
+            if (inputValue != NULL) {
+                
+                for(int l=0; l<14;l++){
+                    touchInfo[l] = touchInfo[l]-5;
+                    if(touchInfo[l] < 0) touchInfo[l] = 0;
+                }
+
+
+                int inputSize = inputValue->size();
+                for(int i=0; i<inputSize; i++){                                                     // inputValue is a list of contacts. A contact is a list layout described at
+                    contactList = inputValue->get(i).asList();                                      // http://wiki.icub.org/wiki/Tactile_sensors_(aka_Skin)
+                    bodyPart = contactList->get(0).asList()->get(3).asInt();                        // retrieve touched body part
+
+                    int taxellCount = contactList->get(6).asList()->size();
+                    taxellList.resize(taxellCount);                                                 // retrieve list of contact taxells
+                    for(int j=0; j<taxellCount; j++){                                             
+                        taxellList[j] = contactList->get(6).asList()->get(j).asInt(); 
+                    }
+        
+                    geometricCenter.resize(3);
+                    for(int j=0; j<3; j++){                 
+                        geometricCenter[j] = contactList->get(4).asList()->get(j).asDouble();        // retrieve the coordinates of contact point
+                    }
+
+                    avgPressure = contactList->get(7).asDouble();                                    // retrieve average pressure of contact       
+                    
+                    Bottle& b = outputDataPort.prepare();                                            //preparing data output bottle
+                    b.clear();
+
+                    switch(bodyPart){                                                                               //retrieve touched sub-part
+                        case (7):                                                                                   // torso
+                            if(geometricCenter[2] >= 0.0 && geometricCenter[1] >= -0.08){ 
+                                touchedPart = 1;                                                                    // torso upright        1
+                                b.addVocab(VOCAB_TORSO_UPRIGHT);
+                            }
+                            else if(geometricCenter[2] < 0.0 && geometricCenter[1] >= -0.08){ 
+                                touchedPart = 2;                                                                    // torso upleft         2
+                                b.addVocab(VOCAB_TORSO_UPLEFT);
+                            }
+                            else if(geometricCenter[2] >= 0.0 && geometricCenter[1] < -0.08){
+                                touchedPart = 3;                                                                    // torso downright      3
+                                b.addVocab(VOCAB_TORSO_DOWNRIGHT);
+                            }
+                            else if(geometricCenter[2] < 0.0 && geometricCenter[1] < -0.08){
+                                touchedPart = 4;                                                                    // torso downleft       4
+                                b.addVocab(VOCAB_TORSO_DOWNLEFT);
+                            }
+                            break; 
+                        case (6):                                                                                   // rightarm upper
+                            if(geometricCenter[2] >= 0.0){
+                                touchedPart = 5;                                                                    // rightarm upright     5
+                                b.addVocab(VOCAB_RIGHTARM_UPRIGHT);
+                            }                                          
+                            else if(geometricCenter[2] < 0.0){ 
+                                touchedPart = 6;                                                                    // rightarm upleft      6
+                                b.addVocab(VOCAB_RIGHTARM_UPLEFT);
+                            }
+                            break;
+                        case (5):                                                                                   // rightarm forearm
+                            if(geometricCenter[0] <= 0.0){ 
+                                touchedPart = 7;                                                                    // rightarm downright   7
+                                b.addVocab(VOCAB_RIGHTARM_FORERIGHT);
+                            }
+                            else if(geometricCenter[0] > 0.0){
+                                touchedPart = 8;                                                                    // rightarm downleft    8
+                                b.addVocab(VOCAB_RIGHTARM_FORELEFT);
+                            }
+                            break;
+                        case (4):
+                            touchedPart = 9;                                                                        // right hand           9
+                            b.addVocab(VOCAB_RIGHTARM_HAND);
+                            break;
+                        case (3):                                                                                   // leftarm upper
+                            if(geometricCenter[2] >= 0.0){ 
+                                touchedPart = 10;                                                                   // leftarm upright     10
+                                b.addVocab(VOCAB_LEFTARM_UPRIGHT);
+                            }
+                            else if(geometricCenter[2] < 0.0){
+                                 touchedPart = 11;                                                                  // leftarm upleft      11
+                                 b.addVocab(VOCAB_LEFTARM_UPLEFT);
+                            }
+                            break;
+                        case (2):                                                                                   // leftarm forearm
+                            if(geometricCenter[0] <= 0.0){ 
+                                touchedPart = 12;                                                                   // leftarm downright   12
+                                b.addVocab(VOCAB_LEFTARM_FORERIGHT);
+                            }
+                            else if(geometricCenter[0] > 0.0){ 
+                                touchedPart = 13;                                                                   // leftarm downleft    13
+                                b.addVocab(VOCAB_LEFTARM_FORELEFT);
+                            }
+                            break;
+                        case (1):
+                            touchedPart = 14;                                                                       // left hand           14
+                            b.addVocab(VOCAB_LEFTARM_HAND);
+                            break;  
+                    }
+                    
+                    if(taxellCount > 3){
+                        touchInfo[touchedPart-1] = touchInfo[touchedPart-1] + 10 + avgPressure/10;
+                        if(touchInfo[touchedPart-1] >= 255){
+                            touchInfo[touchedPart-1] = 255;
+                        }
+                        b.addInt(taxellCount);                                                      //add taxell number to output  
+                        b.addDouble(avgPressure);                                                   //addaverage pressure to output 
+    
+                        if(outputDataPort.getOutputCount()) {
+                            if(useLeftArm && touchedPart >=10){ 
+                                outputDataPort.write();
+                            }else if(useRightArm && touchedPart <10 && touchedPart >=5){ 
+                                outputDataPort.write();
+                            }else if(useTorso && touchedPart <5){ 
+                                outputDataPort.write();
+                            }
+                            
+                        }                
+                    }
+                    
+                    drawContact();
+
+                    if(outputImagePort.getOutputCount()) {
+                        outputImagePort.write();
+                    }                                                  
+                }                 
+            }            
+        }       
+    //}  
+}
+
+int skinProcessorRateThread::processing(){
+    return true;
+}
+
+
+void skinProcessorRateThread::drawContact() {
+    
+    //drawing the white rectangles container
+    rectangle(  image, Point(200, 150), Point(300, 250), Scalar(255, 255, 255), 2, 8   );             // torso upright      1
+    rectangle(  image, Point(300, 150), Point(400, 250), Scalar(255, 255, 255), 2, 8   );             // torso upleft       2
+    rectangle(  image, Point(200, 250), Point(300, 350), Scalar(255, 255, 255), 2, 8   );             // torso downright    3
+    rectangle(  image, Point(300, 250), Point(400, 350), Scalar(255, 255, 255), 2, 8   );             // torso downleft     4
+
+    rectangle(  image, Point(100, 150), Point(125, 250), Scalar(255, 255, 255), 2, 8   );             // rightarm upright   5
+    rectangle(  image, Point(125, 150), Point(150, 250), Scalar(255, 255, 255), 2, 8   );             // rightarm upleft    6
+    rectangle(  image, Point(100, 300), Point(125, 400), Scalar(255, 255, 255), 2, 8   );             // rightarm downright 7
+    rectangle(  image, Point(125, 300), Point(150, 400), Scalar(255, 255, 255), 2, 8   );             // rightarm downleft  8
+    rectangle(  image, Point(100, 450), Point(150, 500), Scalar(255, 255, 255), 2, 8   );             // right hand         9
+
+    rectangle(  image, Point(450, 150), Point(475, 250), Scalar(255, 255, 255), 2, 8   );             // leftarm upright    10
+    rectangle(  image, Point(475, 150), Point(500, 250), Scalar(255, 255, 255), 2, 8   );             // leftarm upleft     11
+    rectangle(  image, Point(450, 300), Point(475, 400), Scalar(255, 255, 255), 2, 8   );             // leftarm downright  12
+    rectangle(  image, Point(475, 300), Point(500, 400), Scalar(255, 255, 255), 2, 8   );             // leftarm downleft   13
+    rectangle(  image, Point(450, 450), Point(500, 500), Scalar(255, 255, 255), 2, 8   );             // left hand          14
+
+    //drawing the intensity of touch
+    if(useTorso){ 
+        rectangle(  image, Point(200, 150), Point(300, 250), Scalar(0, 0, touchInfo[0]), -1, 8   );             // torso upright      1
+        rectangle(  image, Point(300, 150), Point(400, 250), Scalar(0, 0, touchInfo[1]), -1, 8   );             // torso upleft       2
+        rectangle(  image, Point(200, 250), Point(300, 350), Scalar(0, 0, touchInfo[2]), -1, 8   );             // torso downright    3
+        rectangle(  image, Point(300, 250), Point(400, 350), Scalar(0, 0, touchInfo[3]), -1, 8   );             // torso downleft     4
+    }    
+    if(useRightArm){ 
+        rectangle(  image, Point(100, 150), Point(125, 250), Scalar(0, 0, touchInfo[4]), -1, 8   );             // rightarm upright   5
+        rectangle(  image, Point(125, 150), Point(150, 250), Scalar(0, 0, touchInfo[5]), -1, 8   );             // rightarm upleft    6
+        rectangle(  image, Point(100, 300), Point(125, 400), Scalar(0, 0, touchInfo[6]), -1, 8   );             // rightarm downright 7
+        rectangle(  image, Point(125, 300), Point(150, 400), Scalar(0, 0, touchInfo[7]), -1, 8   );             // rightarm downleft  8
+        rectangle(  image, Point(100, 450), Point(150, 500), Scalar(0, 0, touchInfo[8]), -1, 8   );             // right hand         9
+    }
+    if(useLeftArm){ 
+        rectangle(  image, Point(450, 150), Point(475, 250), Scalar(0, 0, touchInfo[9]), -1, 8   );             // leftarm upright    10
+        rectangle(  image, Point(475, 150), Point(500, 250), Scalar(0, 0, touchInfo[10]), -1, 8   );             // leftarm upleft     11
+        rectangle(  image, Point(450, 300), Point(475, 400), Scalar(0, 0, touchInfo[11]), -1, 8   );             // leftarm downright  12
+        rectangle(  image, Point(475, 300), Point(500, 400), Scalar(0, 0, touchInfo[12]), -1, 8   );             // leftarm downleft   13
+        rectangle(  image, Point(450, 450), Point(500, 500), Scalar(0, 0, touchInfo[13]), -1, 8   );             // left hand          14
+    }
+    touchedPart = 0;
+    //imshow( "Skin", image );
+    //waitKey( 1 );
+
+    IplImage Ipl = (IplImage) image; 
+    outputImage = &outputImagePort.prepare();        //preparing the  image for output
+    outputImage->resize(600, 600);
+    outputImage->wrapIplImage(&Ipl);
+}
+
+void skinProcessorRateThread::threadRelease() {
+    // nothing    
+}
+
+void skinProcessorRateThread::onStop() {
+    
+    yInfo("closing the ports");
+    inputPort.interrupt();
+    outputImagePort.interrupt();
+    outputDataPort.interrupt();
+    inputPort.close();
+    outputImagePort.close();
+    outputDataPort.close();
+}
+
