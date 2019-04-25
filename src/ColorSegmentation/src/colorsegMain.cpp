139cd46f9c0f15f292ca1780c2e535c1992f65a5
kzmssk <kzmssk1599@yahoo.co.jp>
2019-04-25 15:12:31 +0200

    Add colorSegmentation module

---
+++ src/ColorSegmentation/src/colorsegMain.cpp
@@ -0,0 +1,23 @@
+#include "darwin/colorsegModule.h"
+
+using namespace std;
+using namespace Darwin::colorseg;
+
+int main(int argc, char* argv[])
+{
+	Network yarp;
+	if (!yarp.checkNetwork())
+        return -1;
+
+	colorsegModule colorseg;
+
+	ResourceFinder rf;
+	rf.setVerbose(true); // print to a console conf info
+	rf.setDefaultConfigFile("colorsegConfiguration.ini");
+	rf.setDefaultContext("ColorSegmentation"); // dove sono i file .ini
+	rf.configure(argc,argv);
+
+	colorseg.runModule(rf);
+
+	return 0;
+}
