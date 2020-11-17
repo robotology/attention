e2944ee0aa7ce380936dc2af112830af4a77d576
Omar Khaled Elsayed Mohamed Eldardeer <omar.eldardeer@iit.it>
2020-11-17 14:02:59 +0100

    send hot point condition changing

--- src/multiSensory/attentionManager/src/attentionManagerThread.cpp
+++ src/multiSensory/attentionManager/src/attentionManagerThread.cpp
@@ -134,7 +134,12 @@ void attentionManagerThread::run() {
             threeSigmaVal =maxValue-meanVal-3*stdVal;
             publishAnalysis();
             yInfo("Max= %d  Mean = %.3f , std = %0.3f var = %.3f  (max-mean)-3s = %0.3f",maxValue,meanVal,stdVal,var,threeSigmaVal);
-            if((maxValue >= max_thresholdVal) && (meanVal <= mean_thresholdVal) && (stdVal >= std_thresholdVal) && (threeSigmaVal >= threeSigma_thresholdVal)){
+//            if((maxValue >= max_thresholdVal) && (meanVal <= mean_thresholdVal) && (stdVal >= std_thresholdVal) && (threeSigmaVal >= threeSigma_thresholdVal)){
+//                if(!sendMaxPointToLinker(idxOfMax,maxValue,meanVal,stdVal)){
+//                    yDebug("max point port not connected to any output");
+//                }
+//            }
+            if(stdVal >= std_thresholdVal){
                 if(!sendMaxPointToLinker(idxOfMax,maxValue,meanVal,stdVal)){
                     yDebug("max point port not connected to any output");
                 }
